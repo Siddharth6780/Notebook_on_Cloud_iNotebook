f77f13800e2f5a56cdd356915ed37397fa986ef8
Siddharth6780 <siddharth6780@gmail.com>
2021-09-17 17:53:48 +0530

    Added Signup functionality

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,7 +4,7 @@ const { body, validationResult } = require("express-validator");
 const User = require("../models/User");
 var bcrypt = require("bcryptjs");
 var jwt = require("jsonwebtoken");
-var fetchuser = require('../middleware/fetchuser')
+var fetchuser = require("../middleware/fetchuser");
 
 const JWT_SECREAT = "MyNameisSID";
 
@@ -17,18 +17,17 @@ router.post(
     body("password", "Enter a Valid password").isLength({ min: 5 }),
   ],
   async (req, res) => {
-
     let success = false;
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ success, errors: errors.array() });
     }
 
     try {
       let user = await User.findOne({ email: req.body.email });
       if (user) {
-        return res.status(400).json({ error: "Email already in use" });
+        return res.status(400).json({ success, error: "Email already in use" });
       }
       const salt = await bcrypt.genSalt(10);
       const secpassword = await bcrypt.hash(req.body.password, salt);
@@ -43,7 +42,8 @@ router.post(
         },
       };
       const authtoken = jwt.sign(data, JWT_SECREAT);
-      res.json({ authtoken });
+      success = true;
+      res.json({ success, authtoken });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Some Error occured");
@@ -61,19 +61,19 @@ router.post(
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ success, errors: errors.array() });
     }
 
     const { email, password } = req.body;
     try {
       let user = await User.findOne({ email });
       if (!user) {
-        return res.status(400).json({ error: "Wrong Crendiatials" });
+        return res.status(400).json({ success, error: "Wrong Crendiatials" });
       }
 
       const passwordCompare = await bcrypt.compare(password, user.password);
       if (!passwordCompare) {
-        return res.status(400).json({ error: "Wrong Crendiatials" });
+        return res.status(400).json({ success, error: "Wrong Crendiatials" });
       }
       const payload = {
         user: {
--- src/components/signup.js
+++ src/components/signup.js
@@ -1,11 +1,104 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Signup = () => {
-    return (
-        <div>
-            Welcome to Signup.
-        </div>
-    )
-}
+  const [credentials, setcredentials] = useState({
+    name: "",
+    email: "",
+    password: "",
+    epassword: "",
+  });
+  let history = useHistory();
+  const handleSummit = async (e) => {
+    e.preventDefault();
+    const { name, email, password } = credentials;
+    const response = await fetch("http://localhost:5000/api/auth/createuser", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email, password }),
+    });
+    const json = await response.json();
+    console.log(json);
+    if (json.success === true) {
+      //redirect
+      localStorage.setItem("token", json.authtoken);
+      history.push("/");
+    } else {
+      alert("Wrong");
+    }
+  };
 
-export default Signup
+  const onChange = (e) => {
+    setcredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSummit}>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+            required
+            name="name"
+            onChange={onChange}
+            type="text"
+            className="form-control"
+            id="name"
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input
+            required
+            name="email"
+            onChange={onChange}
+            type="email"
+            className="form-control"
+            id="email"
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            minLength={5}
+            required
+            name="password"
+            onChange={onChange}
+            type="password"
+            className="form-control"
+            id="password"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="cpassword" className="form-label">
+            Confirm Password
+          </label>
+          <input
+            minLength={5}
+            required
+            name="epassword"
+            onChange={onChange}
+            type="password"
+            className="form-control"
+            id="cpassword"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
